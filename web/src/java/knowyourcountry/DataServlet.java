0cb25c0123e7139c5f3e22aa017df50b0fc76ac2
Rob McAuliffe <robert@mcauliffe.id.au>
2013-06-02 04:00:15 +1000

    we have JSON

--- web/src/java/knowyourcountry/DataServlet.java
+++ web/src/java/knowyourcountry/DataServlet.java
@@ -8,12 +8,14 @@ import com.sun.org.apache.xerces.internal.dom.ElementNSImpl;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -31,6 +33,8 @@ import javax.xml.xpath.XPathFactory;
 import knowyourcountry.abs.ABSStatX0020SDMXX0020WebX0020Service;
 import knowyourcountry.abs.GetGenericData;
 import knowyourcountry.abs.GetGenericDataResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -42,9 +46,42 @@ import org.xml.sax.SAXException;
  */
 public class DataServlet extends HttpServlet
 {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+    private NamespaceContext namespaces = new NamespaceContext() {
+
+        @Override
+        public String getNamespaceURI(String prefix)
+        {
+            String uri = null;
+            if (prefix.equals("m"))
+            {
+                System.err.println("namespace:m");
+                uri = "http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message";
+            }
+            else if (prefix.equals("n"))
+            {
+                System.err.println("namespace:n");
+                uri = "http://www.SDMX.org/resources/SDMXML/schemas/v2_0/generic";
+            }
+            return uri;
+        }
+
+        @Override
+        public String getPrefix(String string)
+        {
+            throw new UnsupportedOperationException("Not needed.");
+        }
+
+        @Override
+        public Iterator getPrefixes(String string)
+        {
+            throw new UnsupportedOperationException("Not needed.");
+        }
+    };
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
+        resp.setContentType("application/json");
         PrintWriter out = new PrintWriter(resp.getOutputStream());
         
         ABSStatX0020SDMXX0020WebX0020Service service = new ABSStatX0020SDMXX0020WebX0020Service();
@@ -58,7 +95,22 @@ public class DataServlet extends HttpServlet
         GetGenericDataResponse.GetGenericDataResult result
             = service.getABSStatX0020SDMXX0020WebX0020ServiceSoap().getGenericData(qm);
         
-        out.print(toString((Node) result.getAny()));
+        Node body = (Node) result.getAny();
+        //Node match = xpathToNode("/m:MessageGroup", body, namespaces);
+        NodeList match = xpathToNodeList("/m:MessageGroup/n:DataSet/n:Series", body, namespaces);
+        JSONArray ja = new JSONArray();
+        for (int i = 0; i < match.getLength(); i++)
+        {
+            String value = xpathToString("n:Obs/n:ObsValue/@value", match.item(i), namespaces);
+            if (!value.equals("NaN"))
+            {
+                JSONObject jo = new JSONObject();
+                jo.put("ASGC_2008", xpathToString("n:SeriesKey/n:Value[@concept='ASGC_2008']/@value", match.item(i), namespaces));
+                jo.put("value", value);
+                ja.add(jo);
+            }
+        }
+        out.print(ja.toJSONString());
         out.flush();
         out.close();
     }
@@ -98,11 +150,40 @@ public class DataServlet extends HttpServlet
         
     }
     
-    private NodeList xpathToNodeList(String expression, Node target) throws ServletException
+    private String xpathToString(String expression, Node target, NamespaceContext namespaces) 
+            throws ServletException
     {
         try
         {
-            XPathExpression xpe = newXpathExpression(expression);
+            XPathExpression xpe = newXpathExpression(expression, namespaces);
+            return (String) xpe.evaluate(target, XPathConstants.STRING);
+        }
+        catch (XPathExpressionException e)
+        {
+            throw new ServletException(e);
+        }
+    }
+    
+    private Node xpathToNode(String expression, Node target, NamespaceContext namespaces) 
+            throws ServletException
+    {
+        try
+        {
+            XPathExpression xpe = newXpathExpression(expression, namespaces);
+            return (Node) xpe.evaluate(target, XPathConstants.NODE);
+        }
+        catch (XPathExpressionException e)
+        {
+            throw new ServletException(e);
+        }
+    }
+    
+    private NodeList xpathToNodeList(String expression, Node target, NamespaceContext namespaces) 
+            throws ServletException
+    {
+        try
+        {
+            XPathExpression xpe = newXpathExpression(expression, namespaces);
             return (NodeList) xpe.evaluate(target, XPathConstants.NODESET);
         }
         catch (XPathExpressionException e)
@@ -136,11 +217,13 @@ public class DataServlet extends HttpServlet
         }
     }
 
-    private XPathExpression newXpathExpression(String expression) throws XPathExpressionException
+    private XPathExpression newXpathExpression(String expression, NamespaceContext namespaces) 
+            throws XPathExpressionException
     {
         XPathExpression xpe;
         XPathFactory xpf = XPathFactory.newInstance();
         XPath xp = xpf.newXPath();
+        xp.setNamespaceContext(namespaces);
         xpe = xp.compile(expression);
         return xpe;
     }
